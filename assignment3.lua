3ade8125e6ae6d546fb8215bc76dd65f39facda4
Lukas Froehlich <lukasfroehlich1@gmail.com>
2016-05-24 13:20:00 -0700

    ready for presentation. multi args works

--- assignment3.lua
+++ assignment3.lua
@@ -108,15 +108,20 @@ function BoolV:getType()
   return "BoolV"
 end
 
+
+-- Set to represent binop values
 function Set (list)
   local set = {}
   for _, l in ipairs(list) do set[l] = true end
   return set
 end
 
-local binop = Set {"+", "-", "*", "/"}
+
+-- binop contains all of the binop forms
+local binop = Set {"+", "-", "*", "/", "<=", "eq?"}
 
 
+-- len computes the length of a table
 function len(T)
   local count = 0
   for _ in pairs(T) do count = count + 1 end
@@ -142,7 +147,18 @@ end
 --assert(check_exn(dummy, 3))
 -------------------
 
---assert(interp(parse({{{"lam", "a", {"+", "a", 1}}, 3}}), {}).val == 4, "appC test")
+-- parse_args consumes a list of args and parses each element
+function parse_args(args) 
+    result = {}
+
+    for i=1,len(args) do
+        table.insert(result, parse({args[i]}))
+    end
+    
+    return result
+end
+
+-- consumes an sexp and returns a ExprC
 function parse(sexp) 
 	if type(sexp[1]) == "number" then
 		return NumC:new(sexp[1])
@@ -162,8 +178,8 @@ function parse(sexp)
         elseif sexp[1][1] == "lam" then
             return LamC:new(sexp[1][2], parse({sexp[1][3]}))
         elseif len(sexp[1]) == 2 then
-            return AppC:new(parse({sexp[1][1]}), 
-                            parse({sexp[1][2]}))
+            return AppC:new(parse({sexp[1][1]}),
+                            parse_args(sexp[1][2]))
         end
 	end
 end
@@ -184,25 +200,23 @@ assert(binop_example.name == "+" and
        binop_example.left.val == 3 and
        binop_example.right.val == 4, "BinopC test")
 
-lam_example = parse({{"lam", "a", {"+", "a", "a"}}})
-assert(lam_example.params == "a" and
+lam_example = parse({{"lam", {"a"}, {"+", "a", "a"}}})
+assert(lam_example.params[1] == "a" and
        lam_example.body.name == "+" and
        lam_example.body.left.val == "a" and
        lam_example.body.right.val == "a", "LamC test")
 
-appc_example = parse({{"a", 3}})
+appc_example = parse({{"a", {3}}})
 assert(appc_example.fun.val == "a" and
-       appc_example.args.val == 3, "AppC test")
+       appc_example.args[1].val == 3, "AppC test")
 
-app_lam_example = parse({{{"lam", "a", {"+", "a", 1}}, 3}})
+app_lam_example = parse({{{"lam", "a", {"+", "a", 1}}, {3}}})
 assert(app_lam_example.fun.params == "a" and
        app_lam_example.fun.body.name == "+" and
-       app_lam_example.args.val == 3, "app_lam test")
+       app_lam_example.args[1].val == 3, "app_lam test")
 
 
-
-
-
+-- Serialize consumes a value and returns a string representing the value
 function Serialize(value)
     if value:getType() == "BoolV" then
        if value.val == true then
@@ -223,10 +237,23 @@ assert(Serialize(BoolV:new(true)) == "true", "BoolV test")
 assert(Serialize(NumV:new(3)) == "3", "NumV test")
 assert(Serialize(CloV:new(3, 4, 5)) == "procedure", "CloV test")
 
-
+-- perform_binop takes the name of a binop and its arguments,
+-- evaluates and returns the result as a value
 function perform_binop(name, left, right)
-	if left:getType() ~= "NumV" or 
-       right:getType() ~= "NumV" then
+    leftType = left:getType()
+    rightType = right:getType()
+
+    if name == "eq?" then
+        if leftType == "CloV" or rightType == "CloV" then
+            return BoolV:new(false)
+        elseif leftType == rightType and left.val == right.val then
+            return BoolV:new(true)
+        else
+            return BoolV:new(false)
+        end
+    end
+	if leftType ~= "NumV" or 
+       rightType ~= "NumV" then
 		error("DFLY: invalid input")
 	end
     if name == "+" then
@@ -235,6 +262,8 @@ function perform_binop(name, left, right)
 		return NumV:new(left.val * right.val)
     elseif name == "-" then
 		return NumV:new(left.val - right.val)
+    elseif name == "<=" then
+		return BoolV:new(left.val <= right.val)
     elseif name == "/" then
 		if right.val == 0 then
 			error("DFLY: division by zero")
@@ -246,9 +275,18 @@ end
 assert(perform_binop("+", NumV:new(3), NumV:new(5)).val == 8, "+ test")
 assert(perform_binop("-", NumV:new(3), NumV:new(5)).val == -2, "- test")
 assert(perform_binop("*", NumV:new(3), NumV:new(5)).val == 15, "* test")
+assert(perform_binop("<=", NumV:new(3), NumV:new(5)).val == true, "<= true test")
+assert(perform_binop("<=", NumV:new(3), NumV:new(2)).val == false, "<= false test")
 assert(perform_binop("/", NumV:new(6), NumV:new(3)).val == 2, "/ test")
+assert(perform_binop("eq?", NumV:new(4), NumV:new(4)).val == true, "eq true test")
+assert(perform_binop("eq?", NumV:new(5), NumV:new(4)).val == false, "eq false test")
+assert(perform_binop("eq?", BoolV:new(true), NumV:new(4)).val == false, "eq false test")
+assert(perform_binop("eq?", BoolV:new(true), BoolV:new(true)).val == true, "eq true test")
+assert(perform_binop("eq?", BoolV:new(false), BoolV:new(true)).val == false, "eq false test")
+assert(perform_binop("eq?", CloV:new(1, 2, 3), CloV:new(1, 2, 3)).val == false, "eq false test")
 	
 
+-- interp consumes an exprc, environment and returns a value
 function interp(expr, env)
 	e_type = expr:getType()
 
@@ -281,7 +319,12 @@ function interp(expr, env)
         return CloV:new(expr.params, expr.body, env)
     elseif e_type == "AppC" then
         closure = interp(expr.fun, env)
-        closure.env[closure.params] = interp(expr.args, env)
+        if len(closure.params) ~= len(expr.args) then
+            error("DFLY: wrong arity")
+        end
+        for i=1,len(expr.args) do
+            closure.env[closure.params[i]] = interp(expr.args[i], env)
+        end
         return interp(closure.body, closure.env)
     else
 	   error("Not a Value!")
@@ -294,9 +337,27 @@ assert(interp(parse({false})).val == false, "BoolV false test")
 assert(interp(parse({{"if", true, 3, 5}})).val == 3, "IfC true test") 
 assert(interp(parse({{"if", false, 3, 5}})).val == 5, "IfC false test") 
 assert(interp(parse({{"+", 3, 5}})).val == 8, "BinopC + test") 
-assert(interp(parse({{"lam", "a", {"*", 3, "a"}}}), {}).params == "a", "clov test") 
-assert(interp(parse({{"lam", "a", {"*", 3, "a"}}}), {}).body.name == "*", "clov test") 
-assert(interp(parse({{"lam", "a", {"*", 3, "a"}}}), {}).body.left.val == 3, "clov test") 
-assert(interp(parse({{"lam", "a", {"*", 3, "a"}}}), {}).body.right.val == "a", "clov test") 
-assert(interp(parse({{{"lam", "a", {"+", "a", 1}}, 3}}), {}).val == 4, "appC test")
+assert(interp(parse({{"lam", {"a"}, {"*", 3, "a"}}}), {}).params[1] == "a", "clov test") 
+assert(interp(parse({{"lam", {"a"}, {"*", 3, "a"}}}), {}).body.name == "*", "clov test") 
+assert(interp(parse({{"lam", {"a"}, {"*", 3, "a"}}}), {}).body.left.val == 3, "clov test") 
+assert(interp(parse({{"lam", {"a"}, {"*", 3, "a"}}}), {}).body.right.val == "a", "clov test") 
+assert(interp(parse({{{"lam", {"a"}, {"+", "a", 1}}, {3}}}), {}).val == 4, "appC test")
+assert(interp(parse({{{"lam", {"a", "b"}, {"*", "a", "b"}}, {3, 4}}}), {}).val == 12, "appC test")
 
+
+-- top_eval takes in an sexp and returns a string representing the evaluation
+function top_eval(sexp)
+   return Serialize(interp(parse(sexp), {}))
+end
+
+assert(top_eval({3}) == "3", "topeval 3")
+assert(top_eval({{"*", 5, 9}}) == "45", "topeval binop")
+assert(top_eval({{"if", true, 1, 2}}) == "1", "topeval if")
+assert(top_eval({{"if", {"<=", 1, 2}, 1, 2}}) == "1", "topeval if")
+assert(top_eval({{{"lam", {"a", "b"}, {"*", "a", "b"}}, {3, 4}}}) == "12", "appC test")
+assert(top_eval({{{"lam", {"a", "b"}, {"if", {"eq?", "a", "b"},
+                                             "a",
+                                             {"*", "a", "b"}}}, {3, 4}}}) == "12", "appC test")
+assert(top_eval({{{"lam", {"a", "b"}, {"if", {"eq?", "a", "b"},
+                                             "a",
+                                             {"*", "a", "b"}}}, {3, 3}}}) == "3", "appC test")
